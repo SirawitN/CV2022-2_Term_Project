c9a0acfa22cf2afac9475d2e818cafcfec693324
SirawitN <nine_sirawit@hotmail.com>
2023-04-08 20:49:37 +0700

    [ADD] add save log function to Holistic model

--- feature_extractor/mediapipe_holistic.py
+++ feature_extractor/mediapipe_holistic.py
@@ -1,6 +1,6 @@
 import cv2
 import time
-import imutils
+import os
 import numpy as np
 import mediapipe as mp
 from mediapipe.python.solutions.drawing_utils import _normalized_to_pixel_coordinates as denormalize_coordinates
@@ -9,10 +9,9 @@ mp_holistic = mp.solutions.holistic
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
-video_input = './resources/5.068.mp4'
+# video_input = './resources/5.068.mp4'
 # video_input = 0
-
-cap = cv2.VideoCapture(video_input)
+# cap = cv2.VideoCapture(video_input)
 
 
 def get_holistic_model(static_image_mode=False, model_complexity=0, smooth_landmarks=True, enable_segmentation=False, refine_face_landmarks=False, min_detection_confidense=0.5, min_tracking_confidense=0.5):
@@ -55,7 +54,7 @@ for i in range(21):
 class MediapipeHolistic:
     def __init__(self):
         self.holistic = get_holistic_model()
-        self.frame_num = 1
+        self.frame_num = 0
         self.landmarks = [header]
         self.denormalized_landmarks = [header]
 
@@ -74,7 +73,12 @@ class MediapipeHolistic:
 
         return frame, results
 
-    def save_landmarks(self, frame_no, frame_width, frame_height, results):
+    def save_landmarks(self, frame_width, frame_height, results):
+        if (not results.pose_landmarks and not results.left_hand_landmarks and not results.right_landmarks):
+            return
+
+        self.frame_num += 1
+
         pose = np.array([[res.x, res.y] for res in results.pose_landmarks.landmark]).flatten(
         ) if results.pose_landmarks else np.zeros(33*2)
         left_hand = np.array([[res.x, res.y] for res in results.left_hand_landmarks.landmark]).flatten(
@@ -82,7 +86,8 @@ class MediapipeHolistic:
         right_hand = np.array([[res.x, res.y] for res in results.right_hand_landmarks.landmark]).flatten(
         ) if results.right_hand_landmarks else np.zeros(21*2)
 
-        cleaned_pose = [round(p, 3) if (p >= 0 and p <= 1) else 0 for p in pose]
+        cleaned_pose = [round(p, 3) if (p >= 0 and p <= 1)
+                        else 0 for p in pose]
         cleaned_left = [round(p, 3) if (p >= 0 and p <= 1)
                         else 0 for p in left_hand]
         cleaned_right = [round(p, 3) if (p >= 0 and p <= 1)
@@ -92,42 +97,70 @@ class MediapipeHolistic:
         denormalized_left = []
         denormalized_right = []
         for i in range(0, len(cleaned_pose), 2):
-            denormalized = denormalize_coordinates(abs(cleaned_pose[i]), abs(cleaned_pose[i+1]), frame_width, frame_height)
+            denormalized = denormalize_coordinates(abs(cleaned_pose[i]), abs(
+                cleaned_pose[i+1]), frame_width, frame_height)
             denormalized_pose.append(denormalized[0])
             denormalized_pose.append(denormalized[1])
         for i in range(0, len(cleaned_left), 2):
-            denormalized = denormalize_coordinates(abs(cleaned_left[i]), abs(cleaned_left[i+1]), frame_width, frame_height)
+            denormalized = denormalize_coordinates(abs(cleaned_left[i]), abs(
+                cleaned_left[i+1]), frame_width, frame_height)
             denormalized_left.append(denormalized[0])
             denormalized_left.append(denormalized[1])
         for i in range(0, len(cleaned_right), 2):
-            denormalized = denormalize_coordinates(abs(cleaned_right[i]), abs(cleaned_right[i+1]), frame_width, frame_height)
+            denormalized = denormalize_coordinates(abs(cleaned_right[i]), abs(
+                cleaned_right[i+1]), frame_width, frame_height)
             denormalized_right.append(denormalized[0])
             denormalized_right.append(denormalized[1])
 
-        print(len(cleaned_pose), len(denormalized_pose), len(cleaned_left), len(denormalized_left), len(cleaned_right), len(denormalized_right))
+        self.landmarks.append(np.concatenate(
+            ([self.frame_num], cleaned_pose, cleaned_left, cleaned_right)))
+        self.denormalized_landmarks.append(np.concatenate(
+            ([self.frame_num], denormalized_pose, denormalized_left, denormalized_right)))
 
-        # self.landmarks.append(np.concatenate(
-        #     ([frame_no], cleaned_pose, cleaned_left, cleaned_right)))
+    def export_to_csv(self, path='', filename='test'):
+
+        save_dir = os.path.join(path, f'{filename}.csv')
+        denormalized_save_dir = os.path.join(
+            path, f'{filename}_denormalized.csv')
+
+        f = open(save_dir, "w")
+        for i in range(len(self.landmarks)):
+            f.write(f'{",".join(str(l) for l in self.landmarks[i])}\n')
+        f.close()
+
+        f = open(denormalized_save_dir, "w")
+        for i in range(len(self.denormalized_landmarks)):
+            f.write(
+                f'{",".join(str(l) for l in self.denormalized_landmarks[i])}\n')
+        f.close()
+
+    def clear_logs(self):
+        self.landmarks.clear()
+        self.denormalized_landmarks.clear()
+        self.landmarks = [header]
+        self.denormalized_landmarks = [header]
 
 
-holistic = MediapipeHolistic()
+# holistic = MediapipeHolistic()
 
-while cap.isOpened():
-    ret, frame = cap.read()
+# while cap.isOpened():
+#     ret, frame = cap.read()
 
-    if ret:
+#     if ret:
 
-        frame, results = holistic.process(frame)
-        frame_h, frame_w, _ = frame.shape
+#         frame, results = holistic.process(frame)
+#         frame_h, frame_w, _ = frame.shape
 
-        holistic.save_landmarks(0, frame_w, frame_h, results)
+#         holistic.save_landmarks(frame_w, frame_h, results)
 
-        cv2.imshow("Processed Frame", frame)
-        if (cv2.waitKey(1) & 0xFF == ord('q')):
-            break
+#         cv2.imshow("Processed Frame", frame)
+#         if (cv2.waitKey(1) & 0xFF == ord('q')):
+#             break
 
-    else:
-        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+#     else:
+#         # cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+#         break
 
-cap.release()
-cv2.destroyAllWindows()
+# holistic.export_to_csv()
+# cap.release()
+# cv2.destroyAllWindows()
