3addcd24975d4069fc4bcf1cd4e9792497d58cfc
Sahatsarin Pawanna <58083752+Ricecamera@users.noreply.github.com>
2023-04-20 21:13:11 +0700

    update features_extractor to work with new folder tree

--- feature_extractor/features_extractor.py
+++ feature_extractor/features_extractor.py
@@ -16,7 +16,7 @@ from mediapipe_holistic import MediapipeHolistic
 # Examples of detected landmarks are in test.csv and test_denormalized.csv
 
 current_dir = os.getcwd()
-resource_dir = os.path.join(current_dir, 'resources', 'CompVisionDataset1')
+resource_dir = os.path.join(current_dir, 'resources', 'CompVisionDataset')
 
 save_dir = os.path.join(current_dir, 'results')
 holistic_detector = MediapipeHolistic()
@@ -24,76 +24,77 @@ holistic_detector = MediapipeHolistic()
 if not os.path.exists(save_dir):
     os.makedirs(save_dir)
 
-files = os.listdir(resource_dir)
-files = [f for f in files if os.path.isfile(resource_dir+'/'+f)]
+classes = os.listdir(resource_dir)
 stop = False
 
-for file in files:
+for cls in classes:
+    cls_dir = os.path.join(resource_dir, cls)
 
-    file_dir = os.path.join(resource_dir, file)
-    
-    if(os.path.exists(file_dir)):
+    # create save directory
+    save_cls_dir = os.path.join(save_dir, cls)
+    if not os.path.exists(save_cls_dir):
+        os.makedirs(save_cls_dir)
 
-        # img_in = './resources/2566.0520.mp4'
-        # img_in = 0
+    # find each video in the class folder
+    files = os.listdir(cls_dir)
+    files = [f for f in files if os.path.isfile(cls_dir+'/'+f)]
 
-        cap = cv2.VideoCapture(file_dir)
+    for file in files:
 
-        # used to record the time when we processed last frame
-        # prev_frame_time = 0
-
-        # # used to record the time at which we processed current frame
-        # new_frame_time = 0
-
-        # fps = cap.get(cv2.CAP_PROP_FPS)
-        # interval = int(1000/fps)
-
-        # frames = 1
-
-        if cap:
-            while cap.isOpened():
-                success, image = cap.read()
-                if success:
-                    # image = imutils.resize(image, height=240)
-                
-                    processed_frame, results = holistic_detector.process(image)
-                    frame_h, frame_w, _ = image.shape
-                    holistic_detector.save_landmarks(frame_w, frame_h, results)
-
-                    cv2.imshow('Processed frame', processed_frame)
-                    if (cv2.waitKey(1) & 0xFF == ord('q')):
-                        stop = True;
-                        break
-                else:
-                    # print(f'no video, Frames = {frames}, landmarks = {len(landmarks)}')
-                    break
-                    # cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-                    # continue
-
-
-            cap.release()
-            cv2.destroyAllWindows()
-
-        else:
-            print("[Error]: Cannot open a video with the given path. Please try again.")
-
-        if stop:
-            break
-        else:
-            holistic_detector.export_to_csv(save_dir, file[:-4])
-            holistic_detector.clear_logs()
+        file_dir = os.path.join(cls_dir, file)
         
-    else:
-        raise Exception(f'An error occured, the {file_dir} doesn\'t exist.')
-    
+        if(os.path.exists(file_dir)):
 
-# import os
-# current_dir = os.getcwd()
-# resource_dir = os.path.join(current_dir, 'resources', 'CompVisionDataset1')
-# # print(os.path.exists(resource_dir))
-# files = os.listdir(resource_dir)
-# files = [f for f in files if os.path.isfile(resource_dir+'/'+f)]
-# print(files)
-# print(files[0], os.path.exists(os.path.join(resource_dir, files[0])))
+            # img_in = './resources/2566.0520.mp4'
+            # img_in = 0
+
+            cap = cv2.VideoCapture(file_dir)
+
+            # used to record the time when we processed last frame
+            # prev_frame_time = 0
+
+            # # used to record the time at which we processed current frame
+            # new_frame_time = 0
+
+            # fps = cap.get(cv2.CAP_PROP_FPS)
+            # interval = int(1000/fps)
+
+            # frames = 1
+
+            if cap:
+                while cap.isOpened():
+                    success, image = cap.read()
+                    if success:
+                        # image = imutils.resize(image, height=240)
+                    
+                        processed_frame, results = holistic_detector.process(image)
+                        frame_h, frame_w, _ = image.shape
+                        holistic_detector.save_landmarks(frame_w, frame_h, results)
+
+                        cv2.imshow('Processed frame', processed_frame)
+                        if (cv2.waitKey(1) & 0xFF == ord('q')):
+                            stop = True;
+                            break
+                    else:
+                        # print(f'no video, Frames = {frames}, landmarks = {len(landmarks)}')
+                        break
+                        # cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+                        # continue
+
+
+                cap.release()
+                cv2.destroyAllWindows()
+
+            else:
+                print("[Error]: Cannot open a video with the given path. Please try again.")
+
+            if stop:
+                break
+            else:
+                holistic_detector.export_to_csv(save_cls_dir, file[:-4])
+                holistic_detector.clear_logs()
+            
+        else:
+            raise Exception(f'An error occured, the {file_dir} doesn\'t exist.')
 
 
